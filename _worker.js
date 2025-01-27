4a7c3c83cc57fe4e11de07f2b56e953aa7e29099
Mission.Zero <68140049+mingdian68@users.noreply.github.com>
2025-01-28 07:24:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,5 +1,5 @@
-addEventListener('fetch'， event => {
-  event.respondWith(handleRequest(event.request))
+addEventListener('fetch' event => {
+  event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
